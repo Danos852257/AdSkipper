94cbfb37c9733ed1495272ee1cf56818d9038368
chairpotato <chairpotato09@gmail.com>
2025-02-03 14:46:11 -0800

    pluh

--- popup.js
+++ popup.js
@@ -1,8 +1,10 @@
+
 const toggleSwitch = document.getElementById("toggleSwitch");
 
 toggleSwitch.addEventListener('change', (event) => {
     if(event.target.checked){
         console.log("on");
+
     }
     else{
         console.log("off")
