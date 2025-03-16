893f79ea5241deee386dbd4d24952874aed1bfa7
Daniel K <dakahd8@gmail.com>
2025-03-16 14:55:41 -0700

    added comments

--- content.js
+++ content.js
@@ -1,78 +1,55 @@
 const button = document.querySelector('.ytSearchboxComponentSearchButton');
 const searchBar = document.querySelector('.ytSearchboxComponentInput');
 
-
-
-/*
-button.addEventListener("focus", function() {
-    console.log("Input field is focused!");
-});
-*/
-
+//does nothing
 function doNothing(){
     while(true){
         break;
     }
 }
 
+//checks if an element is visible (not used)
 function isVisible(element) {
     if (!element) return false;
     const style = window.getComputedStyle(element);
     return style.display !== "none" && style.visibility !== "hidden" && element.offsetParent !== null;
 }
 
-
+//injects the script from skipAdScript.js into Youtube's DOM
 function injectScript() {
     const script = document.createElement('script');
-    script.src = chrome.runtime.getURL('skipAdScript.js'); // Load an external script
+    script.src = chrome.runtime.getURL('skipAdScript.js');
     document.documentElement.appendChild(script);
 }
 
-
-function ninja(){
-    console.log("99999999999999999999");
-    setTimeout(() => {
-        console.log("Observer disconnected");
-        /*const enterKeyEvent = new KeyboardEvent('keydown', {
-            key: 'Enter',
-            code: 'Enter',
-            keyCode: 13,
-            which: 13,
-            bubbles: true
-        });*/
-        document.dispatchEvent(new KeyboardEvent('keydown', { key: 'Enter', code: 'Enter', keyCode: 13, which: 13,bubbles: true }));
-        document.dispatchEvent(new KeyboardEvent('keyup', { key: 'Enter', code: 'Enter', keyCode: 13, which: 13,bubbles: true }));
-    }, 9000);
-}
-
-
-
-
+//test function from before - technically still works but functionality replaced by popup.js
 var search = function(word){
     if(searchBar){
+        //clicks on the search bar
         searchBar.click();
         searchBar.focus();
         searchBar.value = ""; 
 
+        //types in each letter one at a time
         for (let char of word) {
 
             searchBar.value += char;
             let eventOptions = { bubbles: true, cancelable: true, key: char, char: char };
 
             console.log(eventOptions);
-            // Simulate real keyboard events
             searchBar.dispatchEvent(new KeyboardEvent("keydown", eventOptions));
-            //searchBar.dispatchEvent(new KeyboardEvent("keypress", eventOptions));
             searchBar.dispatchEvent(new KeyboardEvent("keyup", eventOptions));
         }
-        //searchBar.value = (word)
 
+        //waits until done typing
         searchBar.dispatchEvent(new Event('input', { bubbles: true }));
         searchBar.dispatchEvent(new Event('change', { bubbles: true }));
         console.log("searcbar works")
     }
 }
 
+
+//clicks on the search button (also obsolete)
 var searchButton = function(){
     if (button) {
         button.dispatchEvent(new Event('click', { bubbles: true }));
@@ -80,15 +57,12 @@ var searchButton = function(){
     }
 }
 
+
+//test event listener to see if the skipAdButton was being clicked
 button.addEventListener('click', () => {
     console.log("Button was actually clicked!");
     console.log(searchBar.value);
 });
 
-
-
-
-
+//Calls injectScript()
 injectScript();
-
-ninja();
--- skipAdScript.js
+++ skipAdScript.js
@@ -1,30 +1,25 @@
+//The script that is injected into Youtube's Document Object Model (DOM)
 (function() {
+    //print statements for debugging
     console.log('rizzler')
+
     function clickSkipButton() {
         const skipButton = document.querySelector('.ytp-skip-ad-button');
-        if (skipButton) {
+        if (skipButton) { //check if button exists
+            //select the button. Pressing enter now skips the ad
             skipButton.focus();
-            skipButton.dispatchEvent(new FocusEvent('focus', { bubbles: true }));
-            skipButton.click();
-            skipButton.dispatchEvent(new PointerEvent('click', { bubbles: true }));
-            skipButton.dispatchEvent(new PointerEvent('pointerdown', { bubbles: true }));
-            skipButton.dispatchEvent(new PointerEvent('pointerup', { bubbles: true }));
+
+            //Try to press enter (doesn't work)
             window.dispatchEvent(new KeyboardEvent('keydown', { key: 'Enter', bubbles: true }));
             window.dispatchEvent(new KeyboardEvent('keyup', { key: 'Enter', bubbles: true }));
-            /*
-            const enterKeyEvent = new KeyboardEvent('keydown', {
-                key: 'Enter',
-                code: 'Enter',
-                keyCode: 13,
-                which: 13,
-                bubbles: true
-            });
-            window.dispatchEvent(enterKeyEvent);
-            console.log("Sigma");*/
-        } else {
+            console.log("sigma");
+        } 
+        /*
+        else {
             console.log("Womp");
         }
+        */
     }
-
+    //do every second
     setInterval(clickSkipButton, 1000);
 })();
